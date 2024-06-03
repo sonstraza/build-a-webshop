fb505debf8a7f427040ca25a71f6af7585ff374d
alex <alexengineering1027@gmail.com>
2024-06-02 20:11:19 -0700

    test

--- webshop/resources/views/livewire/storefront.blade.php
+++ webshop/resources/views/livewire/storefront.blade.php
@@ -1,6 +1,6 @@
 <div class="grid grid-cols-4 gap-4 mt-12">
     @foreach($this->product as $product)
-        <div class="bg-white rounded-lg shadow p-4">
+        <div class="bg-white rounded-lg shadow p-5">
             <img src="{{ $product->image }}" alt="">
             <h2 class="font-medium text-lg">{{ $product->name }}</h2>
             <span class="text-gray-700 text-sm">{{ $product->price }}</span>
