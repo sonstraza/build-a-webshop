1ee2aab27ac4d58913a5deb10a119a4ac54d46fb
alex <alexengineering1027@gmail.com>
2024-06-02 20:08:34 -0700

    Money Formatting and more style

--- webshop/app/Models/Product.php
+++ webshop/app/Models/Product.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Money\Currency;
+use Money\Money;
 
 class Product extends Model
 {
@@ -19,6 +22,15 @@ class Product extends Model
     public function image(): HasOne
     {
         return $this->hasOne(Image::class)->ofMany('featured', 'max');
+    }
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: function(int $value) {
+                return new Money($value, new Currency('USD'));
+            }
+        );
     }
     protected $fillable = ['name', 'description', 'price'];
 }
--- webshop/resources/views/livewire/storefront.blade.php
+++ webshop/resources/views/livewire/storefront.blade.php
@@ -1,9 +1,9 @@
 <div class="grid grid-cols-4 gap-4 mt-12">
     @foreach($this->product as $product)
-        <div>
+        <div class="bg-white rounded-lg shadow p-4">
             <img src="{{ $product->image }}" alt="">
             <h2 class="font-medium text-lg">{{ $product->name }}</h2>
-            <span class="text-gray-700 text-sm">{{ $product->price/100 }}</span>
+            <span class="text-gray-700 text-sm">{{ $product->price }}</span>
         </div>
     @endforeach
 </div>
