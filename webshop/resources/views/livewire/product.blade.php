a1c56b9bb6cbdf8ab31c806d5beeee2f7f6a1857
alex <alexengineering1027@gmail.com>
2024-06-11 20:13:45 -0700

    more blade issues

--- webshop/resources/views/livewire/product.blade.php
+++ webshop/resources/views/livewire/product.blade.php
@@ -1,16 +1,18 @@
 <div class="grid md:grid-cols-2 gap-10">
     <div>
         <div>
-            <img src="{{ $this->product->image->image_path }}" alt="">
+            <img src="/{{ $this->product->image->image_path }}" alt="">
         </div>
         <div class="grid md:grid-cols-4 gap-4">
             @foreach($this->product->images as $image)
-                <img src="{{ $image->image_path }}" class="rounded" alt="">
+                <img src="/{{ $image->image_path }}" class="rounded" alt="">
             @endforeach
         </div>
     </div>
 
     <div>
-        {{ $this->product->name }};
+        <h1 class="text-3xl font-medium">{{ $this->product->name }};</h1>
+        <div class="text-xl text-gray-700">{{ $this->product->price }};</div>
+        <div class="mt-4">{{ $this->product->description }}</div>
     </div>
 </div>
